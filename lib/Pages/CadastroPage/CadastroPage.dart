9cf01c7c7c51aa082322631a7e939ccecb3e96b2
Israel-Silvestre <israelvaz2705@gmail.com>
2024-07-16 21:22:16 -0300

    refactor: Desing da Tela de Cadastro

--- lib/Pages/CadastroPage/CadastroPage.dart
+++ lib/Pages/CadastroPage/CadastroPage.dart
@@ -1,97 +1,129 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:pa1_activy/Pages/LoginPage/LoginPage.dart';
-import 'package:pa1_activy/dataBase/DataBase.dart'; // Importe o banco de dados aqui
-import 'package:pa1_activy/Model/User/UserDB.dart'; // Importe o modelo de usuário aqui
+import 'package:pa1_activy/dataBase/DataBase.dart';
+import 'package:pa1_activy/Model/User/UserDB.dart';
 
-class CadastroPage extends StatefulWidget {
-  @override
-  _CadastroScreenState createState() => _CadastroScreenState();
-}
-
-class _CadastroScreenState extends State<CadastroPage> {
+class CadastroPage extends StatelessWidget {
   final TextEditingController name = TextEditingController();
   final TextEditingController cpf = TextEditingController();
   final TextEditingController login = TextEditingController();
   bool isAdm = false;
   final TextEditingController password = TextEditingController();
 
-  // Método para salvar o usuário no banco de dados
-  void _saveUser() async {
-    final appDatabase =
-        await $FloorDataBase.databaseBuilder('DataBase.db').build();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cadastro',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.black, Colors.grey[900]!],
+            ),
+          ),
+          child: Stack(
+            children: [
+              Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Image.asset(
+                          'assets/logo.png',
+                          width: 150,
+                          height: 150,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: name,
+                              decoration: InputDecoration(
+                                labelText: 'Nome',
+                                prefixIcon: Icon(Icons.person),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            TextFormField(
+                              controller: cpf,
+                              decoration: InputDecoration(
+                                labelText: 'CPF',
+                                prefixIcon: Icon(Icons.credit_card),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            TextFormField(
+                              controller: login,
+                              decoration: InputDecoration(
+                                labelText: 'Login',
+                                prefixIcon: Icon(Icons.person),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            TextFormField(
+                              controller: password,
+                              decoration: InputDecoration(
+                                labelText: 'Senha',
+                                prefixIcon: Icon(Icons.lock),
+                              ),
+                              obscureText: true,
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              children: [
+                                Checkbox(
+                                  value: isAdm,
+                                  onChanged: (value) {
+                                    isAdm = value!;
+                                  },
+                                ),
+                                Text('Administrador'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () => _saveUser(context),
+                        child: Text('Cadastrar'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _saveUser(BuildContext context) async {
+    final appDatabase = await $FloorDataBase.databaseBuilder('DataBase.db').build();
     final dao = appDatabase.usuarioDao;
 
-    var novoUsuario = UserDB(name.text, int.parse(cpf.text),
-        login.text, isAdm, int.parse(password.text));
+    var novoUsuario = UserDB(
+      name.text,
+      int.parse(cpf.text),
+      login.text,
+      isAdm,
+      int.parse(password.text),
+    );
 
-    dao.insertUser(novoUsuario);
+    await dao.insertUser(novoUsuario);
 
-    // Você pode adicionar alguma lógica adicional após salvar o usuário, como exibir uma mensagem ou navegar para outra tela
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => LoginPage()),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Cadastro de Usuário'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: name,
-              decoration: InputDecoration(labelText: 'Nome'),
-            ),
-            SizedBox(height: 9.0),
-            TextField(
-              controller: cpf,
-              decoration: InputDecoration(labelText: 'cpf'),
-            ),
-            SizedBox(height: 9.0),
-            TextField(
-              controller: login,
-              decoration: InputDecoration(labelText: 'login'),
-            ),
-            SizedBox(height: 9.0),
-            Row(
-              children: [
-                Checkbox(
-                  value: isAdm,
-                  onChanged: (value) {
-                    setState(() {
-                      isAdm = value!;
-                    });
-                  },
-                ),
-                Text('Administrador'),
-              ],
-            ),
-            SizedBox(height: 9.0),
-            TextField(
-              controller: password,
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Senha'),
-            ),
-            SizedBox(height: 45.0),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: _saveUser,
-                child: Text('Cadastrar'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ));
   }
 }
